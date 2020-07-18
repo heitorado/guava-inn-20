cd79fc4a0b73ba12446c07fdc7de1efc951a23f8
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-07-18 16:34:09 -0300

    Fix: Add error message and verifications  to not allow searching when start and end dates are swapped
    Closes #15

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,12 @@
 class ReservationsController < ApplicationController
   def search
-    @should_show_results = params[:start_date].present? &&
-                           params[:end_date].present? &&
-                           params[:number_of_guests].present?
+    @should_show_results = params[:number_of_guests].present? &&
+                           start_and_end_date_present?
+
+    if start_and_end_date_present? && !start_date_is_before_end_date?
+      @should_show_results = false
+      flash[:alert] = "Cannot search: the 'From' date must happen before the 'To' date"
+    end
 
     @available_rooms = if @should_show_results
                          Room.minimum_capacity_of(params[:number_of_guests]) -
@@ -20,7 +24,7 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new(reservation_params)
     if @reservation.save
       redirect_to @reservation.room,
-        notice: "Reservation #{@reservation.code} was successfully created."
+                  notice: "Reservation #{@reservation.code} was successfully created."
     else
       render :new
     end
@@ -30,10 +34,20 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
     redirect_to room_path(@reservation.room),
-      notice: "Reservation #{@reservation.code} was successfully destroyed."
+                notice: "Reservation #{@reservation.code} was successfully destroyed."
   end
 
   private
+
+  def start_and_end_date_present?
+    params[:start_date].present? && params[:end_date].present?
+  end
+
+  def start_date_is_before_end_date?
+    return false unless start_and_end_date_present?
+
+    params[:start_date].to_date.before?(params[:end_date].to_date)
+  end
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :number_of_guests, :guest_name, :room_id)
