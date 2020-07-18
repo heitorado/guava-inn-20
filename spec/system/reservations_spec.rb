6b0fe091fb5df71ab772956e53ca2bc3f4ab6ee0
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-07-18 16:14:03 -0300

    Test: Add tests for reservations#search to not allow swapping of start and end dates

--- spec/system/reservations_spec.rb
+++ spec/system/reservations_spec.rb
@@ -37,6 +37,33 @@ RSpec.describe 'Reservations', type: :system do
       visit search_reservations_path
     end
 
+    context 'when start_date is after end_date' do
+      before do
+        expect(page).to have_content('New Reservation')
+
+        within('form') do
+          fill_in 'start_date', with: 7.days.from_now.strftime('%m/%d/%Y')
+          fill_in 'end_date', with: 7.days.ago.strftime('%m/%d/%Y')
+          select '2', from: 'number_of_guests'
+          click_button 'commit'
+        end
+      end
+
+      it 'shows no results' do
+        expect(page).to have_no_content('Available Rooms')
+        expect(page).to have_no_content('101')
+        expect(page).to have_no_content('102')
+        expect(page).to have_no_content('103')
+        expect(page).to have_no_content('104')
+        expect(page).to have_no_content('105')
+        expect(page).to have_no_content('106')
+      end
+
+      it 'returns an error message' do
+        expect(page).to have_content("the 'From' date must happen before the 'To' date")
+      end
+    end
+
     it 'allows users to search for available rooms with a given capacity in a period' do
       expect(page).to have_content('New Reservation')
 
@@ -98,27 +125,29 @@ RSpec.describe 'Reservations', type: :system do
       end
     end
 
-    it "shows the message 'There are no available rooms for the selected filters' when no rooms are returned" do
-      expect(page).to have_content('New Reservation')
+    context 'when there are no rooms available' do
+      it "shows the message 'There are no available rooms for the selected filters'" do
+        expect(page).to have_content('New Reservation')
 
-      within('form') do
-        fill_in 'start_date', with: '08/14/2020'
-        fill_in 'end_date', with: '08/22/2020'
-        select '6', from: 'number_of_guests'
-        click_button 'commit'
-      end
+        within('form') do
+          fill_in 'start_date', with: '08/14/2020'
+          fill_in 'end_date', with: '08/22/2020'
+          select '6', from: 'number_of_guests'
+          click_button 'commit'
+        end
 
-      expect(page).to have_content('Available Rooms')
+        expect(page).to have_content('Available Rooms')
 
-      within('table') do
-        within('tbody') do
-          expect(page).to have_content('There are no available rooms for the selected filters')
-          expect(page).to have_no_content('101')
-          expect(page).to have_no_content('102')
-          expect(page).to have_no_content('103')
-          expect(page).to have_no_content('104')
-          expect(page).to have_no_content('105')
-          expect(page).to have_no_content('106')
+        within('table') do
+          within('tbody') do
+            expect(page).to have_content('There are no available rooms for the selected filters')
+            expect(page).to have_no_content('101')
+            expect(page).to have_no_content('102')
+            expect(page).to have_no_content('103')
+            expect(page).to have_no_content('104')
+            expect(page).to have_no_content('105')
+            expect(page).to have_no_content('106')
+          end
         end
       end
     end
