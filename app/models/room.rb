701d7ac25d4fec639af7cf615cac05dea261b82a
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-07-18 22:52:55 -0300

    Feat: Add length validation on Room :code field and in the input HTML field
    Closes #19

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,7 @@ class Room < ApplicationRecord
 
   validates_presence_of :code, :capacity
   validates_uniqueness_of :code
+  validates_length_of :code, maximum: 12
   validates_numericality_of :capacity, greater_than: 0, less_than_or_equal_to: 10
 
   after_find :calculate_occupancy_rates
