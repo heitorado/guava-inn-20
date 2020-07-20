c7e62ef189638d8980044bb4acd2b39881eaf87c
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-07-19 21:02:08 -0300

    Test: Refactor Reservation unit tests to use Reservation factory and update Reservation factory

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
   factory :reservation do
-    room { create(:room) }
+    transient do
+      on_room { {} }
+    end
+
+    room
     start_date { rand(0..40).days.ago }
-    end_date { rand(0..40).days.from_now }
+    end_date { rand(1..40).days.from_now }
     guest_name { 'Sybil Fawlty' }
-    number_of_guests { rand(1..room.capacity) }
+    number_of_guests { room.nil? ? rand(1..10) : rand(1..room.capacity) }
+
+    after(:build) do |reservation, evaluator|
+      reservation.room = build(:room, evaluator.on_room) if evaluator.on_room.present?
+    end
   end
 end
