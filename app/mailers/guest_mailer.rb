31f0310387b5c6ab1696a6f745393ea5c2f37464
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-07-24 16:08:12 -0300

    Feat: Add confirmation and cancellation email layouts for reservation and implement notifier methods

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -1,7 +1,11 @@
 class GuestMailer < ApplicationMailer
   def reservation_confirmed
+    @reservation = params[:reservation]
+    mail(to: @reservation.guest_email, subject: '[Guava Inn] Reserva confirmada!')
   end
 
   def reservation_cancelled
+    @reservation = params[:reservation]
+    mail(to: @reservation.guest_email, subject: '[Guava Inn] Reserva cancelada :(')
   end
 end
