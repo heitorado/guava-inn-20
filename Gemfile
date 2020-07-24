0a7b1cf9ddac56999ce2e6903606045296bc522b
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-07-23 21:24:57 -0300

    Dependency: Add and configure codecov gem for uploading to codecov website
    Finally, fixes codecov uploading through circleci!

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 3.29'
+  gem 'codecov', require: false
   gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 3.9'
   gem 'rspec_junit_formatter'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,10 @@ SimpleCov.start :rails do
   end
 end
 
+# Upload to codecov website
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
