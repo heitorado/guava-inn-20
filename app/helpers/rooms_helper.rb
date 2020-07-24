5a5337973f69602a38a23a7e81caa19c10276981
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-07-23 23:56:21 -0300

    Feat: Add a new column on room reservations table for displaying reservation status
    Closes #29

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,7 @@
 module RoomsHelper
+  def get_status_of(reservation)
+    return 'Finished' if reservation.end_date <= Date.today
+    return 'Ongoing'  if reservation.start_date <= Date.today && reservation.end_date >= Date.today
+    return 'Planned'  if reservation.start_date > Date.today
+  end
 end
